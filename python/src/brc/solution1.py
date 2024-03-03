374dd0badaa43636b83175e09694ad25439f2cc8
Mark Todisco <marktodisco@gmail.com>
2024-03-03 13:25:48 -0500

    fixed rounding. all tests passing

--- python/src/brc/solution1.py
+++ python/src/brc/solution1.py
@@ -5,25 +5,25 @@ from brc.rounding import java_round
 
 @dc.dataclass
 class Station:
-    _min: float = dc.field(default=float("+inf"), init=False)
-    _max: float = dc.field(default=float("-inf"), init=False)
-    _sum: float = dc.field(default=0, init=False)
+    _min: int = dc.field(default=int(+1_000_000), init=False)
+    _max: int = dc.field(default=int(-1_000_000), init=False)
+    _sum: int = dc.field(default=0, init=False)
     _count: int = dc.field(default=0, init=False)
 
-    def update(self, x: float) -> None:
+    def update(self, x: int) -> None:
         self._min = min(self._min, x)
         self._max = max(self._max, x)
         self._sum += x
         self._count += 1
 
     def get_min(self) -> float:
-        return self._min
+        return self._min / 10
 
     def get_mean(self) -> float:
-        return java_round(self._sum * 10 / self._count) / 10
+        return java_round(self._sum / self._count) / 10
 
     def get_max(self) -> float:
-        return self._max
+        return self._max / 10
 
     def str(self, name: str) -> str:
         return f"{name}={self.get_min():.1f}/{self.get_mean():.1f}/{self.get_max():.1f}"
@@ -37,7 +37,8 @@ def solve(path: str) -> str:
 
     for line in data:
         name, temperature = line.split(";")
-        stations[name].update(float(temperature))
+        temp_x10 = temperature.replace(".", "")
+        stations[name].update(int(temp_x10))
 
     sorted_stations = dict(sorted(stations.items(), key=lambda x: x[0]))
 
